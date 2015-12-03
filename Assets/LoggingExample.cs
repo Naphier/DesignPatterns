33373aba3844c3a92b3d4592452207406ef6a6a5
Jason Storey <jasonstoreyireland@gmail.com>
2015-12-03 21:14:36 +0000

    Adds Service Locator and Example Custom Logger

---
+++ Assets/LoggingExample.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using Assets.Scripts;
+using UnityEngine;
+
+public class LoggingExample : MonoBehaviour
+{
+    ILogger Logger;
+    int myVariable = 55;
+
+    public bool ColorMessages = false;
+
+    void Start ()
+    {
+
+        if (ColorMessages)
+            Services.Logger = new LoggerConsole {ColorMessages = ColorMessages};
+
+
+	    Logger = Services.Logger;
+
+
+        Logger.Info(this,"Starting Example");
+        Logger.Warn(this, "Things are about to get interesting!");
+        Logger.Error(this, "Things got too interesting.....");
+
+
+        StartCoroutine(TestVariableLogging());
+
+	}
+
+
+    IEnumerator TestVariableLogging()
+    {
+        Logger.Variable(this, "Awesomeness", myVariable);
+
+        for (int i = 0; i < 100; i++)
+            Logger.Variable(this, "Awesomeness", myVariable);
+
+        myVariable = 1000;
+
+        for (int i = 0; i < 100; i++)
+            Logger.Variable(this, "Awesomeness", myVariable);
+
+
+        yield return null;
+    }
+
+}
--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -1,25 +1,30 @@
 ﻿using UnityEngine;
+
 public class Command
 {
-	public virtual void Execute(GameObject actor){}
+    public virtual void Execute(GameObject actor)
+    {
+    }
 }
 
 public class JumpCommand : Command
 {
-	CharacterComponent _actor;
-	public override void Execute(GameObject actor)
-	{
-		if (_actor != actor) _actor = actor.GetComponent<CharacterComponent>();
-		_actor.Jump();
-	}
+    private CharacterComponent _actor;
+
+    public override void Execute(GameObject actor)
+    {
+        if (_actor != actor) _actor = actor.GetComponent<CharacterComponent>();
+        _actor.Jump();
+    }
 }
 
 public class FireCommand : Command
 {
-    CharacterComponent _actor;
-    public override void Execute (GameObject actor)
-	{
+    private CharacterComponent _actor;
+
+    public override void Execute(GameObject actor)
+    {
         if (_actor != actor) _actor = actor.GetComponent<CharacterComponent>();
         _actor.Fire();
-	}
+    }
 }
