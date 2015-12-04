613032318cb0b4f471d1e8385e6b0038c54a2997
Sean Mann <naplandgames@gmail.com>
2015-12-04 13:49:04 -0500

    Command.cs - Removed Command class as it was not an interface or abstract and thus not enforcing any architecture. Also the class name was too generic unless contained in a namespace. - Replaced Command class with CharacterCommand which has the ability to have re-assignable input, a default constructor, and methods and properties that are required for all input commands. - Refactored JumpCommand and FireCommand to reflect changes inherited from CharacterCommand. Note how these derived classes are very minimal and easy to write multiples of.
    InputHandler.cs
    - Minor cleanup. Pet peeve - I really don't like seeing "//Use this for initiialization" and such. Not sure why they bug me so much, but they do.
    - Refactored so that we're using the new CharacterCommands and changed the field names to reflect that they aren't controlled by their input.
    - Update() added a test to show how reassignment of the command's input is now reassignable.
    - HandleInput() - modified so that we now use the input method that is in the command so that input can be reassigned.

--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -1,30 +1,82 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-public class Command
+
+// Since this is not an interface or abstract it should function as our base class
+// with all necessary defaults and commonalities for CharacterCommands
+public class CharacterCommand
 {
+    protected CharacterComponent _actor;
+
+    public delegate bool InputDelegate();
+    protected InputDelegate Input;
+    public InputDelegate DefaultInput { get; private set; }
+
+
+    // This will force each subclass to define a constructor with this minimal parameter.
+    // All character commands need an input.
+    public CharacterCommand(InputDelegate inputDelegate)
+    {
+        if (inputDelegate != null)
+            SetInputKey(inputDelegate);
+    }
+
+    
     public virtual void Execute(GameObject actor)
     {
+        if (_actor != actor)
+            _actor = actor.GetComponent<CharacterComponent>();
+    }
+
+
+    public bool GetInput()
+    {
+        if (Input != null)
+            return Input();
+        
+        return false;
+    }
+
+    public void SetInputKey(InputDelegate action)
+    {
+        Input = null;
+        Input += action;
+
+        if (DefaultInput == null)
+            DefaultInput += action;
     }
 }
 
-public class JumpCommand : Command
-{
-    private CharacterComponent _actor;
 
-    public override void Execute(GameObject actor)
+// Now each of our derived commands are very minimalized and easy to create many of them.
+public class JumpCommand : CharacterCommand
+{
+    //Assign default key if constructor parameter is null
+    public JumpCommand(InputDelegate input) : base(input)
     {
-        if (_actor != actor) _actor = actor.GetComponent<CharacterComponent>();
+        if (input == null)
+            SetInputKey( () => UnityEngine.Input.GetKeyDown(KeyCode.Space));
+    }
+
+	public override void Execute(GameObject actor)
+	{
+        base.Execute(actor);
         _actor.Jump();
-    }
+	}
 }
 
-public class FireCommand : Command
-{
-    private CharacterComponent _actor;
 
-    public override void Execute(GameObject actor)
+public class FireCommand : CharacterCommand
+{
+    //Assign default key if constructor parameter is null
+    public FireCommand(InputDelegate input) : base(input)
     {
-        if (_actor != actor) _actor = actor.GetComponent<CharacterComponent>();
-        _actor.Fire();
+        if (input == null)
+            SetInputKey(() => UnityEngine.Input.GetMouseButton(0));
     }
+
+    public override void Execute (GameObject actor)
+	{
+        base.Execute(actor);
+        _actor.Fire();
+	}
 }
--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,47 +1,59 @@
-﻿using UnityEngine;
+using UnityEngine;
+using System.Collections;
 
 public class InputHandler : MonoBehaviour
 {
-    private Command currentCommand;
 
-    private Command MOUSE_BUTTON0;
+    CharacterCommand fireCommand = new FireCommand(null);
+    CharacterCommand jumpCommand = new JumpCommand(null);
+    CharacterCommand currentCommand;
 
-    public GameObject player;
-    private Command SPACE_BAR;
+	public GameObject player;
 
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
+	void Awake()
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
 
-    // Use this for initialization
-    private void Start()
-    {
-        MOUSE_BUTTON0 = new FireCommand();
-        SPACE_BAR = new JumpCommand();
-    }
+    bool defaultKeys = false;
+	void Update () 
+	{
+		currentCommand = HandleInput();
+		if(currentCommand != null)
+		{
+			currentCommand.Execute(player);
+		}
 
-    // Update is called once per frame
-    private void Update()
-    {
-        currentCommand = HandleInput();
-        if (currentCommand != null)
+        // Test reassignment of keys
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            currentCommand.Execute(player);
-        }
-    }
+            
+            if (!defaultKeys)
+            {
+                KeyCode fireKey = KeyCode.UpArrow;
+                KeyCode jumpKey = KeyCode.LeftControl;
+                
+                Debug.LogWarning("Assigning new keys - jump is: " + jumpKey.ToString() + " --  fire is: " + fireKey);
+                fireCommand.SetInputKey(() => Input.GetKey(fireKey));
+                jumpCommand.SetInputKey(() => Input.GetKeyDown(jumpKey));
+            }
+            else
+            {
+                Debug.LogWarning("Resetting keys to default");
+                fireCommand.SetInputKey(fireCommand.DefaultInput);
+                jumpCommand.SetInputKey(jumpCommand.DefaultInput);
+            }
 
-    private Command HandleInput()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            return MOUSE_BUTTON0;
+            defaultKeys = !defaultKeys;
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            return SPACE_BAR;
-        }
+	}
 
-        return null;
-    }
+
+    CharacterCommand HandleInput()
+	{
+		if(fireCommand.GetInput()) { return fireCommand; }
+		if(jumpCommand.GetInput()) { return jumpCommand; }
+
+		return null;
+	}
 }
